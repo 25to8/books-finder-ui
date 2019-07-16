0f0c180fcce33f0de64e0e084e018e15ca73dd59
cryptoji <kocherginji@gmail.com>
2019-07-16 11:04:15 +0300

    ~ Some fixes/updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { BooksListComponent } from './components/+books/list/list.component';
+import { BooksCardComponent } from './components/+books/card/card.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksListComponent,
+    BooksCardComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'AIzaSyBmiNldsGgJYIa0iL7T2ND_U3DBEXWiA5w',
+  apiUrl: 'https://www.googleapis.com/books/v1'
 };
