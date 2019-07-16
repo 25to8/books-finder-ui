a4ef9499693a4d97f9fb7907ab85147654473066
cryptoji <kocherginji@gmail.com>
2019-07-16 11:03:26 +0300

    + Create BooksList component

---
+++ src/app/components/+books/list/list.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BookModel } from '../../../models/book.model';
+
+@Component({
+  selector: 'app-books-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class BooksListComponent implements OnInit {
+  @Input() title = 'Books';
+  @Input() books: BookModel[];
+
+  @Output() loadMore = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
