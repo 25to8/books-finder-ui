6aa2316a31ebd3e80c92aa654ac4b4e56cddb215
cryptoji <kocherginji@gmail.com>
2019-07-16 20:59:49 +0300

    ~ Api Keys in git is a bad practice

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiKey: 'AIzaSyBmiNldsGgJYIa0iL7T2ND_U3DBEXWiA5w',
-  apiUrl: 'https://www.googleapis.com/books/v1'
+  apiKey: 'AIzaSyBmiNldsGgJYIa0iL7T2ND_U3DBEXWiA5w',  // TODO: this is a bad practice
+  apiUrl: 'https://www.googleapis.com/books/v1',
+  defaultQuery: `You don't know JS`
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiKey: 'AIzaSyBmiNldsGgJYIa0iL7T2ND_U3DBEXWiA5w',
+  apiKey: 'AIzaSyBmiNldsGgJYIa0iL7T2ND_U3DBEXWiA5w', // TODO: this is a bad practice
   apiUrl: 'https://www.googleapis.com/books/v1',
   defaultQuery: `You don't know JS`
 };
